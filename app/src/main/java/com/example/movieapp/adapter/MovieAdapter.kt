30c985a2313940ed131383bf735f992b6f75f505
yigitkemal <40325505+yigitkemal@users.noreply.github.com>
2021-11-05 09:36:59 +0300

    Movie Recyclerview adapter is created.

--- app/src/main/java/com/example/movieapp/adapter/MovieAdapter.kt
+++ app/src/main/java/com/example/movieapp/adapter/MovieAdapter.kt
@@ -1,4 +1,36 @@
 package com.example.movieapp.adapter
 
-class MovieAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.NonNull
+import androidx.recyclerview.widget.RecyclerView
+import com.example.movieapp.databinding.ItemMovieBinding
+import com.example.movieapp.model.Movie
+
+class MovieAdapter(val moviesList: ArrayList<Movie>):
+    RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
+
+    class MovieViewHolder(val binding: ItemMovieBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val binding = ItemMovieBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return MovieViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.binding.textItemMovie.setText(moviesList.get(position).movieName)
+    }
+
+    override fun getItemCount(): Int {
+        return moviesList.size
+    }
+
+    fun updataMoiveList(newMovieList: List<Movie>){
+        moviesList.clear()
+        moviesList.addAll(newMovieList)
+        notifyDataSetChanged()
+    }
+
 }
--- app/src/main/java/com/example/movieapp/model/Model.kt
+++ app/src/main/java/com/example/movieapp/model/Model.kt
@@ -1,2 +1,6 @@
 package com.example.movieapp.model
 
+data class Movie(
+    val movieName: String?,
+    val voteAverage: String?,
+    val descriptionOverview: String?)
