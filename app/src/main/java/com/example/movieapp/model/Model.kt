aeff4b92a83ac6a57db3942a5b1071cbfbbc2c9d
yigitkemal <40325505+yigitkemal@users.noreply.github.com>
2021-12-27 14:22:16 +0300

    models have been changed for room database.

--- app/src/main/java/com/example/movieapp/model/Model.kt
+++ app/src/main/java/com/example/movieapp/model/Model.kt
@@ -1,18 +1,39 @@
 package com.example.movieapp.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity
 data class Movie(
+
+    @ColumnInfo(name="title")
     @SerializedName("title")
     val movieName: String?,
+
+    @ColumnInfo(name = "poster_path")
     @SerializedName("poster_path")
     val moviePosterUrl: String?,
+
+    @ColumnInfo(name = "backdrop_path")
     @SerializedName("backdrop_path")
     val moviePhotoUrl: String?,
+
+    @ColumnInfo(name = "vote_average")
     @SerializedName("vote_average")
     val voteAverage: String?,
+
+    @ColumnInfo(name = "overview")
     @SerializedName("overview")
     val descriptionOverview: String?,
+
+    @ColumnInfo(name = "movie_id")
     @SerializedName("id")
-    val movieId: Int)
+    val movieId: Int){
+
+    @PrimaryKey(autoGenerate = true)
+    var uuid: Int = 0
+
+}
 
--- app/src/main/java/com/example/movieapp/model/MovieDetailModel.kt
+++ app/src/main/java/com/example/movieapp/model/MovieDetailModel.kt
@@ -1,30 +1,63 @@
 package com.example.movieapp.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity
 data class MovieDetailModel(
+
+    @ColumnInfo(name = "id")
     @SerializedName("id")
     val movieId: Int?,
+
+    @ColumnInfo(name = "imdb_id")
     @SerializedName("imdb_id")
     val movieImdbId: String?,
+
+    @ColumnInfo(name = "budget")
     @SerializedName("budget")
     val budget: String?,
+
+    @ColumnInfo(name = "overview")
     @SerializedName("overview")
     val overview: String?,
+
+    @ColumnInfo(name = "popularity")
     @SerializedName("popularity")
     val popularity: String?,
+
+    @ColumnInfo(name = "poster_path")
     @SerializedName("poster_path")
     val poster_path: String?,
+
+    @ColumnInfo(name = "backdrop_path")
     @SerializedName("backdrop_path")
     val backdrop_pat: String?,
+
+    @ColumnInfo(name = "release_date")
     @SerializedName("release_date")
     val release_date: String?,
+
+    @ColumnInfo(name = "tagline")
     @SerializedName("tagline")
     val tagline: String?,
+
+    @ColumnInfo(name = "title")
     @SerializedName("title")
     val title: String?,
+
+    @ColumnInfo(name = "vote_average")
     @SerializedName("vote_average")
     val vote_average: String?,
+
+    @ColumnInfo(name = "runtime")
     @SerializedName("runtime")
     val runtime: Int,
-    )
+    ){
+
+    @PrimaryKey(autoGenerate = true)
+    val uuid: Int = 0
+
+}
