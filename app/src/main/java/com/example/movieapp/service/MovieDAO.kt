b78fd3eb8c4a958dd37a665de08325c96cae3e8b
yigitkemal <40325505+yigitkemal@users.noreply.github.com>
2021-12-27 15:23:49 +0300

    Dao is created.

---
+++ app/src/main/java/com/example/movieapp/service/MovieDAO.kt
@@ -0,0 +1,23 @@
+package com.example.movieapp.service
+
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.movieapp.model.Movie
+
+interface MovieDAO {
+
+    @Insert
+    suspend fun insertAkk(vararg movies: Movie): List<Long>
+
+
+
+    @Query("SELECT * FROM movie")
+    suspend fun getAllMovies(): List<Movie>
+
+    @Query("SELECT * FROM movie WHERE uuid= :movieId")
+    suspend fun getMovie(movieId: Int): Movie
+
+    @Query("DELETE FROM movie")
+    suspend fun deleteAllMovies()
+
+}
